aac167949fac0883b2e313f40c660aedb4306a4d
Rafał <rafcikp2@gmail.com>
2020-07-30 18:43:31 +0200

    add class Anki

--- words_to_anki.py
+++ words_to_anki.py
@@ -1,52 +1,7 @@
-import sys, bs4, requests, openpyxl
+import sys, openpyxl
 from Cleaner import Cleaner
+from Anki import Anki
 
-
-def getPronunciation(word):
-    try:
-        url = 'https://dictionary.cambridge.org/dictionary/english/%s' % (word)
-        dictionarySite = requests.get(url)
-        dictionarySoup = bs4.BeautifulSoup(dictionarySite.text, 'html.parser')
-        pronunciationSoup = dictionarySoup.select('.ipa')
-        pronunciation = [pronunciationSoup[0].getText(),pronunciationSoup[1].getText()]
-        return pronunciation
-    except:
-        return ["",""]
-
-def getPolishMeanings(word):
-    try:
-        #word = word.replace(" ","+")
-        #TODO fix for example for ATM card
-        url = "https://www.diki.pl/slownik-angielskiego?q=%s" % (word)
-        dikiResponse = requests.get(url)
-        dikiSoup = bs4.BeautifulSoup(dikiResponse.text,'html.parser')
-        # print(type(dikiSoup))
-        #TODO fix meanings from links in nav
-        # soup = dikiSoup.find("div", {"class": "diki-results-left-column"})
-        # print(len(soup))
-        # print(soup[0])
-        # soup = soup[0].findAll("a", {"class": ".plainLink"})
-        # print(soup)
-
-        # dikiSoup2 = bs4.BeautifulSoup(soup[0],'html.parser')
-        # print(type(dikiSoup2))
-        # print(soup)
-        # soup = soup.select('.plainLink')
-        # print(soup)
-
-        meaningsSoup = dikiSoup.select('.plainLink')
-        # print(meaningsSoup)
-
-        translation = []
-        for elem in meaningsSoup:
-            translation.append(elem.getText())
-        
-        if translation:
-            return translation
-        else:
-            return [""]
-    except:
-        return [""]
 
 def getFrequency(frequencySheet,word):
     for i in range(4,frequencySheet.max_row):
@@ -59,14 +14,10 @@ def getFrequency(frequencySheet,word):
 cleaner = Cleaner(sys.argv[1])
 cleaner.cleanSubtitles()
 
-# cleanSubtitles()
 wb = openpyxl.load_workbook('frequency.xlsx')
 # print(wb.sheetnames)
 frequencySheet = wb['10000k']
 # print(sheet['A1'].value)
-
-
-
 
 
 subtitles = open('subtitles','r')
@@ -79,8 +30,9 @@ fileLineNumbers = 0
 for line in subtitles:
     for vocab in vocabulary:
         if vocab in line:
-            pronunciation = getPronunciation(vocab)
-            polishMeanings = getPolishMeanings(vocab)
+            anki = Anki(vocab)
+            pronunciation = anki.getPronunciation()
+            polishMeanings = anki.getPolishMeanings()
             frequency = getFrequency(frequencySheet,vocab)
             line = line.replace("\n","")
             #TODO paste all possible meaningsp
@@ -89,7 +41,7 @@ for line in subtitles:
             print(vocab,'added.')
             vocabulary.remove(vocab)
             fileLineNumbers += 1
-print(fileLineNumbers, 'lines added.')
+print(fileLineNumbers, 'rows added.')
 
 if not vocabulary:
     print('Vocabulary which were not added')
