2112b497c7fe531086f06dbb9cfb909169e5d0a6
Rafał <rafcikp2@gmail.com>
2021-02-27 08:03:48 +0100

    add requirements

--- Cleaner.py
+++ Cleaner.py
@@ -10,18 +10,16 @@ class Cleaner:
         subtitlesBefore = open(self.filename, "r")
         subtitlesCleaned = open('subtitles', 'w')
 
-        # viriable to remember first part of a sentence
+        # remember first part of a sentence
         lineBefore = ""
         for line in subtitlesBefore:
             line = self.clean_line_regexp(line)
 
-            if not line:
+            if line == "":
                 continue
-
             if lineBefore:
                 line = lineBefore + ' ' + line
                 lineBefore = ""
-
             if line[-2] == '.' or line[-2] == '?' or line[-2] == '!':
                 subtitlesCleaned.write(line)
             else:
