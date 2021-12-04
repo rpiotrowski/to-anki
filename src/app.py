612591e03d73bdcbe7fb43fbeddd7b922b1f16fa
Rafał <rafcikp2@gmail.com>
2021-12-04 13:06:13 +0100

    Refactor

--- src/app.py
+++ src/app.py
@@ -12,27 +12,27 @@ wb = openpyxl.load_workbook('src/frequency.xlsx')
 frequencySheet = wb['10000k']
 
 subtitles = open('subtitles', 'r')
-to_anki = open('to_anki', 'w')
 
 vocabulary = list(input("Enter vocabularies separated by colons\n").split(','))
 comment = input("Enter comment you want to add\n")
 
 fileLineNumbers = 0
-for line in subtitles:
-    for vocab in vocabulary:
-        if vocab in line:
-            anki = Anki(vocab)
-            pronunciation = anki.get_pronunciation()
-            polishMeanings = anki.get_polish_meanings()
-            frequency = anki.get_frequency(frequencySheet, vocab)
-            line = line.replace("\n", "")
-            # TODO paste all possible meaningsp
-            to_anki.write(polishMeanings[0] + ';;' + vocab + ';' + line + ';' + comment + ';' + pronunciation[0] + ';' +
-                          pronunciation[1] + ';' + frequency + '\n')
-            # Only find first sentence with that vocabulary
-            print(vocab, 'added.')
-            vocabulary.remove(vocab)
-            fileLineNumbers += 1
+with open('output/to_anki', 'w') as to_anki:
+    for line in subtitles:
+        for vocab in vocabulary:
+            if vocab in line:
+                anki = Anki(vocab)
+                pronunciation = anki.get_pronunciation()
+                polishMeanings = anki.get_polish_meanings()
+                frequency = anki.get_frequency(frequencySheet, vocab)
+                line = line.replace("\n", "")
+                # TODO paste all possible meaningsp
+                to_anki.write(polishMeanings[0] + ';;' + vocab + ';' + line + ';' + comment + ';' + pronunciation[0] + ';' +
+                              pronunciation[1] + ';' + frequency + '\n')
+                # Only find first sentence with that vocabulary
+                print(vocab, 'added.')
+                vocabulary.remove(vocab)
+                fileLineNumbers += 1
 print(fileLineNumbers, 'rows added.')
 
 if not vocabulary:
